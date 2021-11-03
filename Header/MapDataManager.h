2d7424f5d16e327682f981ad7849c8459adec89b
kimgeonsu <49184941+kimgeonsu@users.noreply.github.com>
2021-11-03 15:12:11 +0900

    Add files via upload

---
+++ Header/MapDataManager.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <iostream>
+#include <experimental/filesystem>
+#include "MyType.h"
+#include "DesignRecordManager.h"
+#include "DrawData.h"
+
+namespace fs = std::experimental::filesystem;
+
+class MapDataManager {
+public:
+	MapDataManager();
+	~MapDataManager();
+
+private:
+	std::string _mapDataPath;
+
+public:
+	long Init();
+	long LoadMapData() {
+		std::string searchPath;
+		Point parcelID;
+		std::string fileName;
+	
+		searchPath = _mapDataPath + "\\Lv1\\0_0";
+
+		for (const fs::directory_entry& entry : fs::directory_iterator(searchPath)) {
+			fileName = entry.path().string();
+			ReadFile(fileName, &_drawData);
+			std::cout << fileName << std::endl;
+		}
+
+		std::string designFileName;
+		designFileName = _mapDataPath + "\\Lv1_info.ddf";
+		_designRecordMng.LoadDesignFile(designFileName);
+
+		return true;
+	}
+
+	void setMapDataPath(std::string mapDataPath) {
+		_mapDataPath = mapDataPath;
+	}
+	std::string GetMapDataPath() {
+		return _mapDataPath;
+	}
+
+	long ReadFile(std::string fileName, CDrawData *drawData);
+
+public :
+	CDrawData _drawData;
+	CDesignRecordMng _designRecordMng;
+};
