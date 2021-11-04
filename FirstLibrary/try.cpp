2ad81478c271181834b5bf3c07f448f5a8d5bc05
kimgeonsu <kundoo0412@gmail.com>
2021-11-04 16:51:44 +0900

    과연 될까유

---
+++ FirstLibrary/try.cpp
@@ -0,0 +1,47 @@
+// trytry.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <linux/types.h>
+#include <linux/ioctl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+extern "C" {
+#include "./include/type.h"
+#include "./include/pen.h"
+#include "./include/brush.h"
+#include "./include/graphics.h"
+}
+
+#include "MapDrawEngine.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    std::string dataFilePath = "linux-map/FirstLibray/Output";
+
+    ////---------------------------------------------------
+    CMapDrawEngine *engine = new CMapDrawEngine;
+
+    engine->Init();
+    engine->SetMapDataPath(dataFilePath);
+
+    cout << engine->GetDeviceRect().left << endl;
+    
+    Graphics* graphics = new Graphics("dev/fb0");
+    if (engine->  DrawMap(graphics))
+        cout << "I'm True hahahaha\n";
+
+    return 0;
+}
