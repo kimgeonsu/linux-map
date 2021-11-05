3bd335d5f4d8a0fbfe48b51fe80119b316076ece
kimgeonsu <kundoo0412@gmail.com>
2021-11-05 15:30:33 +0900

    zxzj

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -81,8 +81,9 @@ long CMapDrawEngine::DrawMap()
 
 	std::list<_MapRecord>::iterator pos = mapDataManager._drawData._drawDataList.begin();
 	_MapRecord pData;
+	std::cout << *pos.header.objType << std:endl
 	
-	for (pos; pos != 10; pos++) {
+	for (pos; pos != mapDataManager._drawData._drawDataList.end(); pos++) {
 		pData = *pos;
 
 		switch (pData.header.objType)
