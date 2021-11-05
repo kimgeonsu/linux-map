8079644d4e866df2d9512cbd79cdc0f05e8c008d
kimgeonsu <kundoo0412@gmail.com>
2021-11-05 16:07:35 +0900

    A

--- FirstLibrary/try.cpp
+++ FirstLibrary/try.cpp
@@ -49,7 +49,7 @@ int main()
     cout << engine->GetDeviceRect().left << endl;
     
     engine->DrawMap();
-    engine->WorldToDevice(PointF(128.5, 36.5));
+    engine->WorldToDevice(_dPoint(128.5, 36.5));
     engine->DrawMap();
     
     return 0;
