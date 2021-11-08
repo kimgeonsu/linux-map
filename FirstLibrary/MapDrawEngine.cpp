9d0e53f6defa2400be754bb0baf3598e94cbc3ee
kimgeonsu <kundoo0412@gmail.com>
2021-11-08 13:36:30 +0900

    gh

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -379,7 +379,7 @@ long CMapDrawEngine::DrawPolygon(_MapRecord* pData, double angle, long bufferIdx
 		tmp.Y = textRect.TopLeft().y;
 		std::cout << "textData가 문제인가? 4\n";
 		std::cout << textData << std::endl;
-		graphics->DrawString(pData->header.textData, -1, &font, tmp, &fillBrush);
+		// graphics->DrawString(pData->header.textData, -1, &font, tmp, &fillBrush);
 		//SetTextColor(hDC, oldTextColor);
 		std::cout << "textData가 문제인가? 5\n";
 
