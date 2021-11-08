f191c11907ba23fe73d9fee8af632d6f82d8ce17
kimgeonsu <kundoo0412@gmail.com>
2021-11-08 14:28:39 +0900

    aa

--- FirstLibrary/DrawData.cpp
+++ FirstLibrary/DrawData.cpp
@@ -35,7 +35,6 @@ void CDrawData::AddDataList(_MapRecord* inData)
 	}
 
 	if (isInsert == false) {
-		std::cout << *inData->header.textData << std::endl;
 		_drawDataList.push_back(*inData);
 	}
 	return;
