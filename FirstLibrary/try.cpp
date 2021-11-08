b56c4bd77e0cf3759e4740551ef6e7dd78470586
kimgeonsu <kundoo0412@gmail.com>
2021-11-08 16:09:07 +0900

    ㅁㄴㅇ

--- FirstLibrary/try.cpp
+++ FirstLibrary/try.cpp
@@ -36,8 +36,8 @@ int main()
     
     engine->Init();
     engine->SetMapDataPath(dataFilePath);
-	engine->SetDeviceRect(0, 0, 480, 800);
-	engine->SetDeviceCenterPos(240, 400);
+	engine->SetDeviceRect(0, 0, 480, 700);
+	engine->SetDeviceCenterPos(240, 350);
 	engine->SetZoomLevel(0);
 	engine->SetMapPos(128.0, 36.0);
 
