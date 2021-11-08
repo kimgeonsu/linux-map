a4b1d9d0a69f1a6e65b2e04ef094edaaa018eb73
kimgeonsu <kundoo0412@gmail.com>
2021-11-08 10:51:22 +0900

    aa

--- FirstLibrary/DrawData.cpp
+++ FirstLibrary/DrawData.cpp
@@ -30,8 +30,6 @@ void CDrawData::AddDataList(_MapRecord* inData)
 		std::cout << "we're same~~\n";
 	for (; listPos != end; listPos++) {
 		//pData = *listPos;
-		std::cout << "여기도 안되려나.......\n";
-		std::cout << "drawOrder : " << inData->header.drawOrder << std::endl;
 		if (listPos->header.drawOrder > inData->header.drawOrder) {
 			_drawDataList.push_front(*inData);
 			isInsert = true;
