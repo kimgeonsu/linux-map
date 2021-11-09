1ee5e38179cd865b17bd163d4fec85077f3f7330
kimgeonsu <kundoo0412@gmail.com>
2021-11-09 12:28:07 +0900

    dasdasd

--- FirstLibrary/DrawData.cpp
+++ FirstLibrary/DrawData.cpp
@@ -20,6 +20,7 @@ void CDrawData::Init()
 
 void CDrawData::AddDataList(_MapRecord* inData)
 {
+	std::cout << "AddDataList\n";
 	std::list<_MapRecord>::iterator	listPos = _drawDataList.begin();
 	std::list<_MapRecord>::iterator end = _drawDataList.end(); 
 	//_MapRecord pData;
@@ -28,6 +29,7 @@ void CDrawData::AddDataList(_MapRecord* inData)
 	for (; listPos != end; listPos++) {
 		//pData = *listPos;
 		if (listPos->header.drawOrder > inData->header.drawOrder) {
+			std::cout << "You? 1\n";
 			_drawDataList.push_front(*inData);
 			isInsert = true;
 			break;
@@ -35,6 +37,7 @@ void CDrawData::AddDataList(_MapRecord* inData)
 	}
 
 	if (isInsert == false) {
+			std::cout << "You? 2\n";
 		_drawDataList.push_back(*inData);
 	}
 	return;
