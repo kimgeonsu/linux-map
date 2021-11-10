b6a5cf92f81c06f05625b037db2641a52e2ce393
kimgeonsu <kundoo0412@gmail.com>
2021-11-10 11:00:52 +0900

    df

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -189,7 +189,7 @@ bool CMapDrawEngine::IsDrawObject(Rect drawRect, Rect objRect)
 long CMapDrawEngine::DrawPolygon(_MapRecord* pData, double angle, long bufferIdx)
 {
 	// for intersect calculation..
-	Rect		objRect(pData->header.boundaryRect);
+	// Rect		objRect(pData->header.boundaryRect);
 	Rect		drawRect;
 	Rect		checkRect;
 	Rect		rectBuffer;
