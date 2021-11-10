1618968d6ca0f5f4d65e6e808e582b38db829dfd
kimgeonsu <kundoo0412@gmail.com>
2021-11-10 13:37:50 +0900

    dasdas

--- FirstLibrary/try.cpp
+++ FirstLibrary/try.cpp
@@ -35,7 +35,7 @@ int main()
     engine->SetMapDataPath(dataFilePath);
 	engine->SetDeviceRect(0, 0, 480, 800);
 	engine->SetDeviceCenterPos(240, 400);
-	engine->SetZoomLevel(1);
+	engine->SetZoomLevel(0);
 	engine->SetMapPos(127.0, 37.0);
 
 	int currentAngle = 90;
