94a6839d3ae498b33f46ff1e9c6d4b10c287a4d7
kimgeonsu <kundoo0412@gmail.com>
2021-12-15 09:50:02 +0900

    eqweqwq

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -296,8 +296,8 @@ long CMapDrawEngine::DrawPolygon(_MapRecord* pData, double angle, long bufferIdx
 	{
 		oldBrush = m_NULLBRUSH;
 	}
+	graphics->FillPolygon(&(fillBrush), g_DrawBuffer, nIdx);
 	graphics->DrawPolygon(&(drawPen), g_DrawBuffer, nIdx);
-	// graphics->FillPolygon(&(fillBrush), g_DrawBuffer, nIdx);
 
 	// Text Output
 	if ((textData.length() > 0) && (designInfo != nullptr))
