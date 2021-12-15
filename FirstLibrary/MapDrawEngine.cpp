f2978fb45a3177289ac08db57ce1709cb4ed257d
kimgeonsu <kundoo0412@gmail.com>
2021-12-15 14:30:37 +0900

    dasdas

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -181,7 +181,7 @@ long CMapDrawEngine::DrawPolygon(_MapRecord* pData, double angle, long bufferIdx
 	Point		textSize;
 	std::string		textData;
 
-	Color color = Color(0,0,256);
+	Color color = Color(255,0,0);
 	Brush oldBrush = Brush(color);
 	Brush fillBrush = Brush(color);
 	Pen	drawPen = Pen(color, 1.0);
