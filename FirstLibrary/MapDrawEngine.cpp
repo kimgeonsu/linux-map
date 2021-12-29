e224f5f5b38916bd465bd98536ed6783655ccef1
kimgeonsu <kundoo0412@gmail.com>
2021-12-29 18:06:30 +0900

    adsdas

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -322,7 +322,7 @@ long CMapDrawEngine::DrawPolygon(_MapRecord* pData, double angle, long bufferIdx
 		}
 		if (haveMinus == false) {
 
-			std::cout << g_DrawBuffer[0].X << " , " << g_DrawBuffer[0].Y << nIdx << std::endl;
+			std::cout << g_DrawBuffer[0].X << " , " << g_DrawBuffer[0].Y << "  " << nIdx << std::endl;
 			std::cout << g_DrawBuffer[nIdx-2].X << " , " << g_DrawBuffer[nIdx-2].Y << std::endl;
 			graphics->FillPolygon(&(fillBrush), g_DrawBuffer, nIdx-1);
 		}
