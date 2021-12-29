d7ee1ce7a825b56e77f6b0dcef89a273b26280d6
kimgeonsu <kundoo0412@gmail.com>
2021-12-29 17:56:42 +0900

    dasdas

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -320,7 +320,7 @@ long CMapDrawEngine::DrawPolygon(_MapRecord* pData, double angle, long bufferIdx
 			}
 		}
 		if (haveMinus == false) {
-			graphics->FillPolygon(&(fillBrush), g_DrawBuffer, nIdx);
+			// graphics->FillPolygon(&(fillBrush), g_DrawBuffer, nIdx);
 		}
 		else
 		{
