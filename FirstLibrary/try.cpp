375cd700db4e5f84a2a90a6b1bf45b7ec2fd50dc
kimgeonsu <kundoo0412@gmail.com>
2021-12-30 12:02:40 +0900

    dasd

--- FirstLibrary/try.cpp
+++ FirstLibrary/try.cpp
@@ -24,6 +24,73 @@ extern "C" {
 
 using namespace std;
 
+Graphics 	*graphics;
+Color 		 color(0, 0, 0);
+Pen 		 pen(color, 1.0);
+Brush 		 brush(color);
+
+void polyline()
+{
+	PointF points[6];
+	PointF point;
+	int i;
+	int x = 10;
+	int y = 110;
+	int width = 100;
+	int height = 100;
+	float rx;
+	float ry;
+	float cx;
+	float cy;
+	double theta;
+	double dtheta;
+	rx = ((x + width) - x) / 2;
+	ry = (y - (y + height)) / 2;
+	cx = x + rx;
+	cy = y + ry;
+	theta = (-M_PI) / 2;
+	dtheta = (4 * M_PI) / 5;
+	for (i = 0; i < 5; i++) {
+		point.X = cx + (rx * cos(theta));
+		point.Y = cy + (ry * sin(theta));
+		points[i] = point;
+		theta += dtheta;
+	}
+
+	graphics->DrawLines(&pen, points, 5);
+}
+
+void polygon(int px, int py, int pw, int ph)
+{
+	PointF points[6];
+	PointF point;
+	int i;
+	int x = px;
+	int y = py;
+	int width = pw;
+	int height = ph;
+	float rx;
+	float ry;
+	float cx;
+	float cy;
+	double theta;
+	double dtheta;
+	rx = ((x + width) - x) / 2;
+	ry = (y - (y + height)) / 2;
+	cx = x + rx;
+	cy = y + ry;
+	theta = (-M_PI) / 2;
+	dtheta = (4 * M_PI) / 5;
+	for (i = 0; i < 5; i++) {
+		point.X = cx + (rx * cos(theta));
+		point.Y = cy + (ry * sin(theta));
+		points[i] = point;
+		theta += dtheta;
+	}
+
+	graphics->DrawPolygon(&pen, points, 5);
+}
+
 int main()
 {
     // std::string dataFilePath = "../Output";
@@ -69,40 +136,74 @@ int main()
     //     }
 
     // }
-
-    Graphics 	*graphics;
-	Color 		 color(255, 192, 0);
-	Pen 		 pen(color, 1.0);
-
-	graphics = new Graphics("/dev/fb0");
+    
+    graphics = new Graphics("/dev/fb0");
 
 	//Screen clear.
 	Brush clear(Color(255, 255, 255));
 	graphics->FillRectangle(&clear, RectF(0, 0, 800, 480));
 	//
 
-	graphics->DrawLine(&pen, PointF(10, 10), PointF(110, 10));
-	graphics->DrawLine(&pen, PointF(110, 10), PointF(110, 110));
-	graphics->DrawLine(&pen, PointF(110, 110), PointF(10, 110));
-	graphics->DrawLine(&pen, PointF(10, 110), PointF(10, 10));
+	//star
+	polyline();
+	polygon(120, 110, 100, 100);
+	//
 
-	pen.SetWidth(2.0);
-	graphics->DrawLine(&pen, PointF(10+150, 10), PointF(110+150, 10));
-	pen.SetWidth(3.0);
-	graphics->DrawLine(&pen, PointF(110+150, 10), PointF(110+150, 110));
-	pen.SetWidth(4.0);
-	graphics->DrawLine(&pen, PointF(110+150, 110), PointF(10+150, 110));
-	pen.SetWidth(5.0);
-	graphics->DrawLine(&pen, PointF(10+150, 110), PointF(10+150, 10));
+	////hexagon
+	int x = 10;
+	int y = 120;
 
-	pen.SetWidth(11.0);
-	graphics->DrawLine(&pen, PointF(10, 200), PointF(10+100, 300));
-	graphics->DrawLine(&pen, PointF(10+200+50, 300), PointF(10+100+50, 200));
-	graphics->DrawLine(&pen, PointF(10+200+100, 300), PointF(10+300+100, 200));
-	graphics->DrawLine(&pen, PointF(10+400+150, 200), PointF(10+300+150, 300));
+	PointF hexa_pts[] = {
+		PointF(x+30, y), 
+		PointF(x, y+50), 
+		PointF(x+30, y+100), 
+		PointF(x+80, y+100), 
+		PointF(x+110, y+50), 
+		PointF(x+80, y)
+	};
+
+	graphics->FillPolygon(&brush, hexa_pts, 6);
+	graphics->DrawPolygon(&pen, hexa_pts, 6);
+	//
+
+	//triangle
+	brush.SetColor(Color(0,255,0));
+	pen.SetColor(Color(255,0,0));
+
+	x = 200;
+	y = 100;
+
+	PointF triangle_pts[] = {PointF(x, y), PointF(x-100, y+200), PointF(x+50, y+20)};
+
+	graphics->FillPolygon(&brush, triangle_pts, 3);
+	graphics->DrawPolygon(&pen, triangle_pts, 3);
+
+	pen.SetColor(Color(0,0,0));
+	//
+
+	//fox
+	brush.SetColor(Color(255,127,39));
+
+	x = 250;
+	y = 120;
+
+	PointF fox_pts[] = {
+		PointF(x+30, y), 
+		PointF(x, y+50), 
+		PointF(x+55, y+100), 
+		PointF(x+110, y+50), 
+		PointF(x+80, y),
+		PointF(x+70, y+25),
+		PointF(x+40, y+25)
+	};
+
+	graphics->FillPolygon(&brush, fox_pts, 7);
+	graphics->DrawPolygon(&pen, fox_pts, 7);
+	//
 
 	graphics->~Graphics();
 
 	return 0;
+
 
 }
