9ee5a58cba943e7a55420db6ebbedfb2c677263b
kimgeonsu <kundoo0412@gmail.com>
2021-12-30 11:57:06 +0900

    aaaa

--- FirstLibrary/try.cpp
+++ FirstLibrary/try.cpp
@@ -24,8 +24,6 @@ extern "C" {
 
 using namespace std;
 
-eun Menu { MOVE, ANGLE, EXIT }
-
 int main()
 {
     // std::string dataFilePath = "../Output";
@@ -52,27 +50,59 @@ int main()
     // engine->DeviceToWorldMove(ip, ip);
     // engine->DrawMap();
 
-    string cmd;
-    while (1) {
-        cout << ">";
-        cin >> cmd;
+    // string cmd;
+    // while (1) {
+    //     cout << ">";
+    //     cin >> cmd;
 
-        
+    //     if (cmd == "move") {
 
-        switch (menu) {
-            case MOVE:
-                /* code */
-                break;
-            case EXIT:
+    //     }
+    //     if (cmd == "rotate") {
 
-            default:
-                break;
-        }
-    }
+    //     }
+    //     if (cmd == "zoom") {
 
-    
+    //     }
+    //     if (cmd == "exit") {
+    //         return 0;
+    //     }
 
+    // }
 
+    Graphics 	*graphics;
+	Color 		 color(0, 0, 0);
+	Pen 		 pen(color, 1.0);
 
-    return 0;
+	graphics = new Graphics("/dev/fb0");
+
+	//Screen clear.
+	Brush clear(Color(255, 255, 255));
+	graphics->FillRectangle(&clear, RectF(0, 0, 800, 480));
+	//
+
+	graphics->DrawLine(&pen, PointF(10, 10), PointF(110, 10));
+	graphics->DrawLine(&pen, PointF(110, 10), PointF(110, 110));
+	graphics->DrawLine(&pen, PointF(110, 110), PointF(10, 110));
+	graphics->DrawLine(&pen, PointF(10, 110), PointF(10, 10));
+
+	pen.SetWidth(2.0);
+	graphics->DrawLine(&pen, PointF(10+150, 10), PointF(110+150, 10));
+	pen.SetWidth(3.0);
+	graphics->DrawLine(&pen, PointF(110+150, 10), PointF(110+150, 110));
+	pen.SetWidth(4.0);
+	graphics->DrawLine(&pen, PointF(110+150, 110), PointF(10+150, 110));
+	pen.SetWidth(5.0);
+	graphics->DrawLine(&pen, PointF(10+150, 110), PointF(10+150, 10));
+
+	pen.SetWidth(11.0);
+	graphics->DrawLine(&pen, PointF(10, 200), PointF(10+100, 300));
+	graphics->DrawLine(&pen, PointF(10+200+50, 300), PointF(10+100+50, 200));
+	graphics->DrawLine(&pen, PointF(10+200+100, 300), PointF(10+300+100, 200));
+	graphics->DrawLine(&pen, PointF(10+400+150, 200), PointF(10+300+150, 300));
+
+	graphics->~Graphics();
+
+	return 0;
+
 }
