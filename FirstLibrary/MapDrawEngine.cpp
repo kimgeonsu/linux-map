4f569929271be6e5f0436d3ccdfad59932011c6b
kimgeonsu <kundoo0412@gmail.com>
2021-12-31 10:13:24 +0900

    aaa

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -79,7 +79,8 @@ long CMapDrawEngine::DrawMap()
 			DrawPolyline(&pData, drawInfo.mapAngle, nIdx);
 			break;
 		case 5:
-			DrawPolygon(&pData, drawInfo.mapAngle, nIdx);
+			if (pData.header.designCode > 20)
+				DrawPolygon(&pData, drawInfo.mapAngle, nIdx);
 			std::cout << pData.header.designCode << std::endl;
 			break;
 		default:
