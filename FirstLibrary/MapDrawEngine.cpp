0518cfbf5b10775e7be6dbc6238df747430bd0c9
kimgeonsu <kundoo0412@gmail.com>
2021-12-31 10:29:29 +0900

    aaaaaaa

--- FirstLibrary/MapDrawEngine.cpp
+++ FirstLibrary/MapDrawEngine.cpp
@@ -701,7 +701,7 @@ Color CMapDrawEngine::colorConverter(int data) {
 	int g = hexa[4] * 16 + hexa[5];
 	int b = hexa[2] * 16 + hexa[3];
 
-	std::cout << std::hex << decimal << std::endl;
-	std::cout << r << " " << g << " " << b << std::endl;
+	std::cout << "16진수 : "<< std::hex << decimal << std::endl;
+	std::cout << "RGB : " << r << " " << g << " " << b << std::endl;
 	return Color(r, g, b);
 }
