a1c374cf8bd1f8a4fc9764256e1cc39c19f04ef7
kimgeonsu <kundoo0412@gmail.com>
2022-01-03 09:29:08 +0900

    dasd

--- FirstLibrary/try.cpp
+++ FirstLibrary/try.cpp
@@ -33,71 +33,78 @@ Brush 		 brush(color);
 
 int main()
 {
-    std::string dataFilePath = "../Output";
-    CMapDrawEngine *engine = new CMapDrawEngine;
-    engine->Init();
-    engine->SetMapDataPath(dataFilePath);
-	engine->SetDeviceRect(0, 0, 480, 800);
-	engine->SetDeviceCenterPos(240, 400);
+    // std::string dataFilePath = "../Output";
+    // CMapDrawEngine *engine = new CMapDrawEngine;
+    // engine->Init();
+    // engine->SetMapDataPath(dataFilePath);
+	// engine->SetDeviceRect(0, 0, 480, 800);
+	// engine->SetDeviceCenterPos(240, 400);
 
-	engine->SetZoomLevel(0);
-	engine->SetMapPos(128.0, 36.0);
+	// engine->SetZoomLevel(0);
+	// engine->SetMapPos(128.0, 36.0);
 
-	int currentAngle = 0;
-	int currentMapMode = 1;
-	engine->SetMapAngle(currentAngle);
-	engine->SetMapHeadingUpMode(currentMapMode);
-    engine->SetMapDayNightMode(0);
+	// int currentAngle = 0;
+	// int currentMapMode = 1;
+	// engine->SetMapAngle(currentAngle);
+	// engine->SetMapHeadingUpMode(currentMapMode);
+    // engine->SetMapDayNightMode(0);
 
-    _dPoint dp;
-    dp.x = 128.5; 
-    dp.y = 36.5;
-    Point ip = Point(10,10);
-    engine->WorldToDevice(dp);
-    engine->DeviceToWorldMove(ip, ip);
-    engine->DrawMap();
+    // _dPoint dp;
+    // dp.x = 128.5; 
+    // dp.y = 36.5;
+    // Point ip = Point(10,10);
+    // engine->WorldToDevice(dp);
+    // engine->DeviceToWorldMove(ip, ip);
+    // engine->DrawMap();
 
-    string cmd;
-	vector<string> commands;
-	double data1, data2;
-    while (1) {
-        cout << "명령어를 입력하세용 : ";
-		getline(cin, cmd);
-		istringstream foo(cmd);
-		string token;
+    // string cmd;
+	// vector<string> commands;
+	// double data1, data2;
+    // while (1) {
+    //     cout << "명령어를 입력하세용 : ";
+	// 	getline(cin, cmd);
+	// 	istringstream foo(cmd);
+	// 	string token;
 
-		while(getline(foo, token, ' ')) {
-			commands.push_back(token);
-		}
+	// 	while(getline(foo, token, ' ')) {
+	// 		commands.push_back(token);
+	// 	}
 
-		cout << cmd << endl;
+	// 	cout << cmd << endl;
 
-        if (commands[0] == "move") {
-			engine->MoveMap(stol(commands[1]), stol(commands[2]));
-			engine->DrawMap();
-        }
-        if (commands[0] == "rotate") {
-			engine->SetMapAngle(stol(commands[1]));
-			engine->DrawMap();
-		}
-		if (commands[0] == "zoom") {
-			engine->SetZoomLevel(stol(commands[1]));
-			engine->DrawMap();
-		}
-		if (commands[0] == "dark") {
-			engine->SetMapDayNightMode(0);
-			engine->DrawMap();
-		}
-		if (commands[0] == "light") {
-			engine->SetMapDayNightMode(1);
-			engine->DrawMap();
-		}
-        if (commands[0] == "exit") {
-			std::cout << "바이 바이 ~~~~\n";
-            return 0;
-        }
-		commands.clear();
-    }
+    //     if (commands[0] == "move") {
+	// 		engine->MoveMap(stol(commands[1]), stol(commands[2]));
+	// 		engine->DrawMap();
+    //     }
+    //     if (commands[0] == "rotate") {
+	// 		engine->SetMapAngle(stol(commands[1]));
+	// 		engine->DrawMap();
+	// 	}
+	// 	if (commands[0] == "zoom") {
+	// 		engine->SetZoomLevel(stol(commands[1]));
+	// 		engine->DrawMap();
+	// 	}
+	// 	if (commands[0] == "dark") {
+	// 		engine->SetMapDayNightMode(0);
+	// 		engine->DrawMap();
+	// 	}
+	// 	if (commands[0] == "light") {
+	// 		engine->SetMapDayNightMode(1);
+	// 		engine->DrawMap();
+	// 	}
+    //     if (commands[0] == "exit") {
+	// 		std::cout << "바이 바이 ~~~~\n";
+    //         return 0;
+    //     }
+	// 	commands.clear();
+    // }
+
+	graphics = new Graphics("/dev/fb0");
+
+	//Screen clear.
+	Brush clear(Color(255, 255, 255));
+	graphics->FillRectangle(&clear, RectF(0, 0, 800, 480));
+
 
 	brush.SetColor(Color(255,127,39));
 
@@ -449,11 +456,10 @@ int main()
 	PointF(-557 , -1791),
 	PointF(-565 , -1799),
 	PointF(891 , -1799),
-
 	};
 
-	graphics->FillPolygon(&brush, fox_pts, fox_pts.length());
-	graphics->DrawPolygon(&pen, fox_pts, fox_pts.length());
+	graphics->FillPolygon(&brush, fox_pts, 347);
+	graphics->DrawPolygon(&pen, fox_pts, 347);
 	//
 
 	graphics->~Graphics();
